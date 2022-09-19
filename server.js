84d1c074292dd91132278a2b9dc18cc709d5d9b8
Obrienjames34 <Obrienjames934@gmail.com>
2022-09-19 13:41:28 -0400

    .gitignore

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+const express = require("express");
 const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require("./controllers"));
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
   {
